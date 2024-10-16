882aed896c5085228ad86bfed397bcdf9c1a7f3b
Alex Bilozor <79287796+bilozorDev@users.noreply.github.com>
2024-10-15 21:12:22 -0700

    feat: add website title and desc functionalityu

--- components/SettingsSlider/generalTab/MetaDataUpdate.tsx
+++ components/SettingsSlider/generalTab/MetaDataUpdate.tsx
@@ -1,8 +1,31 @@
+import { useGeneralSetting } from "@/components/contexts/GeneralTabSettings";
 import useGetBrandColor from "@/components/hooks/useGetBrandColor";
-import React from "react";
+import { useEffect, useState } from "react";
 
 const MetaDataUpdate = () => {
   const brandColor = useGetBrandColor();
+  const { generalSettings, setGeneralSettings } = useGeneralSetting();
+  const [newMetaData, setNewMetaData] = useState({
+    title: generalSettings.metaData.title,
+    description: generalSettings.metaData.description,
+  });
+
+  const handleUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setGeneralSettings({
+      ...generalSettings,
+      metaData: {
+        ...generalSettings.metaData,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+  // update
+  useEffect(() => {
+    if (generalSettings.metaData.title) {
+      document.title = generalSettings.metaData.title;
+    }
+  }, [generalSettings.metaData.title]);
 
   return (
     <form>
@@ -11,15 +34,17 @@ const MetaDataUpdate = () => {
           className={`relative rounded-md rounded-b-none px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-[${brandColor}]`}
         >
           <label
-            htmlFor="name"
+            htmlFor="title"
             className="block text-xs font-medium text-gray-500"
           >
             Website name
           </label>
           <input
-            id="name"
-            name="name"
+            id="title"
+            name="title"
             type="text"
+            value={generalSettings.metaData.title}
+            onChange={handleUpdate}
             placeholder="Website builder"
             className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
@@ -28,26 +53,22 @@ const MetaDataUpdate = () => {
           className={`relative rounded-md rounded-t-none px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-[${brandColor}]`}
         >
           <label
-            htmlFor="desc"
+            htmlFor="description"
             className="block text-xs font-medium text-gray-500"
           >
             Website Description
           </label>
           <input
-            id="desc"
-            name="desc"
+            id="description"
+            name="description"
             type="text"
+            value={generalSettings.metaData.description}
+            onChange={handleUpdate}
             placeholder="Awesome website to build your own website"
             className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
-      <button
-        type="submit"
-        className={`inline-flex mt-5 items-center rounded-md disabled:bg-gray-400 disabled:cursor-not-allowed bg-[${brandColor}] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
-      >
-        Save
-      </button>
     </form>
   );
 };
