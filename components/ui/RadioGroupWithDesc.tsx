76977f36f0768a5f2377842cf9b702565a31ced3
Alex Bilozor <79287796+bilozorDev@users.noreply.github.com>
2024-10-19 19:51:58 -0700

    fix: colors issue on RadioInput

--- components/ui/RadioGroupWithDesc.tsx
+++ components/ui/RadioGroupWithDesc.tsx
@@ -32,20 +32,20 @@ const RadioGroupWithDesc = ({
               optionIdx === options.length - 1
                 ? "rounded-bl-md rounded-br-md"
                 : "",
-              "group relative flex cursor-pointer border border-gray-200 p-4 focus:outline-none data-[checked]:z-10 data-[checked]:border-indigo-200 data-[checked]:bg-indigo-50"
+              "group relative flex cursor-pointer border border-gray-200 p-4 focus:outline-none data-[checked]:z-10 data-[checked]:border-gray-300 data-[checked]:bg-gray-100"
             )}
           >
             <span
               aria-hidden="true"
-              className="mt-0.5 flex h-4 w-4 shrink-0 cursor-pointer items-center justify-center rounded-full border border-gray-300 bg-white group-data-[checked]:border-transparent group-data-[checked]:bg-indigo-600 group-data-[focus]:ring-2 group-data-[focus]:ring-indigo-600 group-data-[focus]:ring-offset-2"
+              className="mt-0.5 flex h-4 w-4 shrink-0 cursor-pointer items-center justify-center rounded-full border border-gray-300 bg-white group-data-[checked]:border-transparent group-data-[checked]:bg-indigo-600 group-data-[focus]:ring-2 group-data-[focus]:ring-gray-600 group-data-[focus]:ring-offset-2"
             >
               <span className="h-1.5 w-1.5 rounded-full bg-white" />
             </span>
             <span className="ml-3 flex flex-col">
-              <span className="block text-sm font-medium text-gray-900 group-data-[checked]:text-indigo-900">
+              <span className="block text-sm font-medium text-gray-900 group-data-[checked]:text-gray-900">
                 {option.name}
               </span>
-              <span className="block text-sm text-gray-500 group-data-[checked]:text-indigo-700">
+              <span className="block text-sm text-gray-500 group-data-[checked]:text-gray-700">
                 {option.description}
               </span>
             </span>
