6124d613c98eba6523a788d66ab17bec384894c6
Alex Bilozor <79287796+bilozorDev@users.noreply.github.com>
2024-10-26 14:29:50 -0700

    fix: eslint

--- components/SettingsSlider/bannerTab/StyleSelector.tsx
+++ components/SettingsSlider/bannerTab/StyleSelector.tsx
@@ -1,13 +1,13 @@
-import { styleOption } from "@/components/@types/styleOption.t";
 import { useBannerSetting } from "@/components/contexts/BannerTabSettings";
 import RadioGroupWithDesc from "@/components/ui/RadioGroupWithDesc";
 import { allAvailableOptions } from "@/components/utils/allAvailableOptions";
+import { SelectionVariation } from "@/components/utils/allAvailableOptions.t";
 import React from "react";
 
 const StyleSelector = () => {
   const { bannerSettings, setBannerSettings } = useBannerSetting();
   const { style } = bannerSettings;
-  const handleUpdate = (e: styleOption) => {
+  const handleUpdate = (e: SelectionVariation) => {
     setBannerSettings({ ...bannerSettings, style: e });
   };
   return (
