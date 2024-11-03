9340ec130b354d88187eecb4fcab1d230bdc3786
Alex Bilozor <79287796+bilozorDev@users.noreply.github.com>
2024-11-02 17:00:02 -0700

    fix: eslint

--- components/Body/Features/FeaturesDefaultStyle.tsx
+++ components/Body/Features/FeaturesDefaultStyle.tsx
@@ -26,6 +26,7 @@ const features = [
   },
 ];
 const FeaturesDefaultStyle = ({ part }: { part: SelectedComponent }) => {
+  console.log(part);
   return (
     <div className="overflow-hidden bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
