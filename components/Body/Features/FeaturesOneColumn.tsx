e4c5e0f19088ae98b76e3718f43f76b6ff6a742a
Alex Bilozor <79287796+bilozorDev@users.noreply.github.com>
2024-11-03 17:36:53 -0800

    fix: estlint

--- components/Body/Features/FeaturesOneColumn.tsx
+++ components/Body/Features/FeaturesOneColumn.tsx
@@ -2,53 +2,6 @@ import useGetBrandColor from "@/components/hooks/useGetBrandColor";
 import IconComponent from "@/components/ui/IconComponent";
 import { SelectedComponent } from "@/components/utils/allAvailableOptions.t";
 import { createDataObject } from "@/components/utils/createDataObject";
-import {
-  ArrowPathIcon,
-  CloudArrowUpIcon,
-  Cog6ToothIcon,
-  FingerPrintIcon,
-  LockClosedIcon,
-  ServerIcon,
-} from "@heroicons/react/20/solid";
-
-const features = [
-  {
-    name: "Push to deploy.",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: "SSL certificates.",
-    description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
-    icon: LockClosedIcon,
-  },
-  {
-    name: "Simple queues.",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.",
-    icon: ArrowPathIcon,
-  },
-  {
-    name: "Advanced security.",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
-    icon: FingerPrintIcon,
-  },
-  {
-    name: "Powerful API.",
-    description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
-    icon: Cog6ToothIcon,
-  },
-  {
-    name: "Database backups.",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ",
-    icon: ServerIcon,
-  },
-];
 import React from "react";
 
 const FeaturesOneColumn = ({ part }: { part: SelectedComponent }) => {
